dccd05f9166c4b2dd19f234516f52717c9cf3fcc
Michal Kukla <michal.kukla@tul.cz>
2023-04-14 10:40:34 +0200

    Added mock button

--- cmd/sti2023/main.go
+++ cmd/sti2023/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"math/rand"
+	"time"
 	//	"io"
 	"github.com/Solamil/sti2023"
 )
 
 func main(){
 	fmt.Printf("%x", sti2023.Hash("michal.kukla@tul.cz"))
+	mockButton("michal.kukla@tul.cz")
 	//sti2023.CurrencyRates()
 	//sti2023.CreatePayment("michal.kukla@tul.cz", 20.0, "in", "GBP")
 	//fmt.Println(sti2023.GetBalances("michal.kukla@tul.cz"))
@@ -23,4 +27,25 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func mockButton(email string) bool {
+	rand.Seed(time.Now().UnixNano())
+	balances := sti2023.GetBalances(email)
+	coinIndex := rand.Intn(len(balances))
+	coinCode := sti2023.GetUserCoinCodes(email)[coinIndex]
+	balance, _ := strconv.ParseFloat(balances[coinIndex], 64)
+	
+	rand.Seed(time.Now().UnixNano())
 
+	direction := "out"
+	if balance < 25 {
+		direction = "in"
+	}
+
+	var total float64 = 0.0 
+	if balance > 1 {
+		total += float64(rand.Intn(int(balance)))
+	}
+	total += rand.Float64() 
+	return sti2023.CreatePayment(email, total, direction, coinCode) 
+	
+}
