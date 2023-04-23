74a1e69106bb635d2974b1a1a865b97a52c23719
Solamil <michal.kukla@tul.cz>
2023-04-23 11:15:45 +0200

    Features/05ui (#14)
    * Reversed list of payments(newest on the top)
    
    * Validate mail address
    
    * index.html, login.html: Cosmetic and functional changes
    
    * Added multiple handlers
    
    * Added accept.html page for accepting payments
    
    * main: Compare strings case insensitive
    
    * Basic Payment
    
    * html: GET -> POST
    
    * Removed validate mail address function

--- cmd/sti2023/main.go
+++ cmd/sti2023/main.go
@@ -6,12 +6,14 @@ import (
 	"strconv"
 	"math/rand"
 	"time"
+	"strings"
 	//	"io"
 	"text/template"
 	"github.com/Solamil/sti2023"
 )
 
 type indexDisplay struct {
+	EmailAddress   string
 	InfoText       string
 	User	       string
 	AddCurrency    string
@@ -19,9 +21,17 @@ type indexDisplay struct {
 	Accounts       string
 	Payments       string
 }
-var indexTemplate *template.Template
 
-var email string = "michal.kukla@tul.cz"
+type acceptDisplay struct {
+	EmailAddress   	string
+	InfoText       	string
+	PayTotal	string
+	PayCoinCode  	string
+	PayDirection 	string
+}
+var indexTemplate *template.Template
+var acceptTemplate *template.Template
+
 
 
 func main(){
@@ -29,23 +39,65 @@ func main(){
 	generateCode()
 
 	//sti2023.WriteCode(email, "sdfa")
-	mockButton(email)
+	//mockButton(email)
 	//sti2023.CurrencyRates()
 	//sti2023.CreatePayment(email, 20.0, "in", "GBP")
 	//fmt.Println(sti2023.GetBalances(email))
 	//sti2023.AddCurrency(email, "GBP")
-	http.HandleFunc("/", index_handler)
 	http.HandleFunc("/index.html", index_handler)
+	http.HandleFunc("/accounts", accounts_handler)
+	http.HandleFunc("/mock", mock_handler)
+	http.HandleFunc("/pay", pay_handler)
+	http.HandleFunc("/", index_handler)
+
 	http.ListenAndServe(":8904", nil)
 }
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
+	var email string = "michal.kukla@tul.cz"
+	
+	var info string = ""
+	fmt.Println(r.URL.Path)
+	if r.URL.Path == "/accept" {
+		email := r.FormValue("email")
+		totalStr := r.FormValue("total")
+		total, _ := strconv.ParseFloat(totalStr, 64)
+		direction := r.FormValue("payment_type")
+		coinCode := r.FormValue("accounts")
+		coinCodePrev := coinCode
+		var balance float64 = 0.0
+		if sti2023.PreparePayment(email, &balance, &total, &direction, &coinCode) {
+			var i acceptDisplay
+			i.EmailAddress = email 
+			i.PayTotal = fmt.Sprintf("%.2f", total) 
+			i.InfoText = ""
+			if strings.EqualFold(direction, "IN") {
+				i.PayDirection = getHTMLOptionTag(direction, "Příchozí platba")
+
+			} else if strings.EqualFold(direction, "OUT") {
+				i.PayDirection = getHTMLOptionTag(direction, "Odchozí platba")
+			}
+			if strings.EqualFold(coinCode, "CZK") && !strings.EqualFold(coinCodePrev, "CZK") {
+				rate := sti2023.GetCurrencyRate(coinCodePrev)
+				date := sti2023.GetDate()
+				i.InfoText = fmt.Sprintf("%s: %s%s %sKč", date, rate[1], coinCodePrev, rate[0])
+			}
+			i.PayCoinCode = getHTMLOptionTag(coinCode, coinCode) 
+			acceptTemplate, _ = template.ParseFiles("web/accept.html")
+			acceptTemplate.Execute(w, i)
+			return
+		} else {
+			info = "Platba se nepodařila provést z důvodu nízkého zůstatku na účtech."
+		}
+
+	}
 
 	user := sti2023.GetNames(email)
 	firstname := user[0]
 	lastname := user[1]
 	var i indexDisplay
-	i.InfoText = ""
+	i.EmailAddress = email
+	i.InfoText = info
 	i.User = firstname+" "+lastname
 	i.AddCurrency = getAddCurrencyHTML(email)
 	i.UserCoinCodes = getUserCoinCodesHTML(email)
@@ -53,6 +105,35 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 	i.Payments = getPaymentsHTML(email)
 	indexTemplate, _ = template.ParseFiles("web/index.html")
 	indexTemplate.Execute(w, i)
+}
+
+func pay_handler(w http.ResponseWriter, r *http.Request) {
+	total, _ := strconv.ParseFloat(r.FormValue("total"), 64)
+	direction := r.FormValue("payment_type")
+	coinCode := r.FormValue("accounts")
+	email := r.FormValue("email")
+	if !sti2023.CreatePayment(email, total, direction, coinCode) {
+		var i acceptDisplay
+		i.InfoText = "Platba se nezdařila."
+		i.PayTotal = fmt.Sprintf("%.2f", total)
+		i.PayDirection = direction
+		i.PayCoinCode = coinCode
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func mock_handler(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	mockButton(email)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func accounts_handler(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	coin := r.FormValue("currencies")
+	sti2023.AddCurrency(email, coin)	
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func mockButton(email string) bool {
@@ -127,16 +208,17 @@ func getPaymentsHTML(email string) string {
 	totals := sti2023.GetPaymentTotal(email)		
 	directions := sti2023.GetPaymentDirection(email)
 	coinCodes := sti2023.GetPaymentCoinCode(email)
-	for i := range totals {
-		if len(directions) <= i || len(coinCodes) <= i {
-			break
-		}
-		if directions[i] == "in" {
+	if len(totals) != len(directions) || len(totals) != len(coinCodes) {
+		return result
+	}
+	for i := len(totals)-1; i >= 0; i-- {
+		if strings.EqualFold(directions[i], "IN") {
 			directions[i] = "+"
-		}else if directions[i] == "out" {
+		}else if strings.EqualFold(directions[i], "out") {
 			directions[i] = "-"
 		}
 		result = fmt.Sprintf("%s<h4 style=\"display: inline-block; margin: 10px;\">%s%s %s</h4>\n", result, directions[i], totals[i], coinCodes[i])
+
 	}
 	return result
 }
