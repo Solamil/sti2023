f72600979b3659b3afeaa5e7267799e3bbd18c43
Michal Kukla <michal.kukla@tul.cz>
2023-04-25 09:54:57 +0200

    Added coverage web page html

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@
 export GOROOT=/usr/local/go
 export PATH=$GOROOT/bin:"$PATH"
 web_dir="web/"
-go test -v -coverprofile ./...
+go test -v -coverprofile cover.out ./...
 go tool cover -html=cover.out -o ${web_dir}cover.html
 kill -2 "$(pgrep main -u "$(whoami)")" 2>&1
 setsid -f go run ./cmd/sti2023/main.go >/dev/null 2>&1
